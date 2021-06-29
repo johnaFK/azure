c8cefe5d83ce5b8b53dc30f37672eb6d58fea18a
Johna <johnatan.jfk@gmail.com>
2021-06-29 16:26:22 -0500

    a

--- CLI Scripts/install-docker-on-linux.sh
+++ CLI Scripts/install-docker-on-linux.sh
@@ -4,7 +4,7 @@
 sudo yum install -y yum-utils
 
 # Add
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 	
 # Install Docker
 sudo yum install -y docker-ce docker-ce-cli containerd.io
