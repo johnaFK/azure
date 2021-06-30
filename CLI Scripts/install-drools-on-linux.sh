fd5f84350e45f8ef0830e9112e0bcca93fb95e15
Johna <johnatan.jfk@gmail.com>
2021-06-30 18:38:57 -0500

    a

--- CLI Scripts/install-drools-on-linux.sh
+++ CLI Scripts/install-drools-on-linux.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo docker run \
-    -p 8080:8080 -p 8001:8001 -d 
-    --name drools-wb 
+    -p 8080:8080 -p 8001:8001 -d \
+    --name drools-wb \
     jboss/business-central-workbench-showcase:latest
 
 
-sudo docker run 
-    -p 8180:8080 -d 
-    --name kie-server 
-    --link drools-wb:kie-wb 
+sudo docker run \
+    -p 8180:8080 -d \
+    --name kie-server \
+    --link drools-wb:kie-wb \
     jboss/kie-server-showcase:latest
