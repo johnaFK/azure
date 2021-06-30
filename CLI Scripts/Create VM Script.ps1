44316139e676e74a32dbfad52260ea5be34b3a85
Johna <johnatan.jfk@gmail.com>
2021-06-30 12:18:39 -0500

    a

--- CLI Scripts/Create VM Script.ps1
+++ CLI Scripts/Create VM Script.ps1
@@ -6,8 +6,9 @@ $ImageUrnAlias="CentOS"
 $StorageType="Standard_LRS"
 $OsDiskSize="64"
 $Size="Standard_B1s"
-$AuthType="ssh"
+$AuthType="all"
 $AdminUserName="linadmin"
+$AdminPassword="abcDEF1029;1"
 $SshPublicKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDQl+aQ/wEjfcFO6GU2f7vWQHbgkSnZiDqIaRoJYSgxR71bXc5TTCfwlBamfl3Y930a6e6VPT/MYEM+gc4qfnulHMGmFOfrDr4qh3yItBa1zC0ZeRluYvYYs7Ujt/qS2rf/ZfxE3R+VAwKVYE+8dMeiEfiY5fPtdxo8cTA3O9f/rvNFf1H+anshNpJ6BR4Dnz4w7Phm5r+zJDkbuIz9+qTQcD3Yin95fmBptv9FK1KvhdVsA9F+ApsyWrC9BYBOWpK3Ee+/OxejlBwjUyh3Z5pc52sFz0H1VyytQzph2xXmcCCxfR9tyg3m4S0q9EdRWsu62hsdZAsZI6vN8lOztk6jC9JQCWw1bn9ocfTghoE7cM4nmZWiy5AZEtDyZVmDjDOJ2Xd1plg/z1f/HNAyEXnmT32vEy0KkUvNntRiarlFOBtSj+K1pBbn5IodKeVk8FdJsmiWArwFoW7N012zTXScCOGzQrxizgZPKZSTo/B5YjZz5FRIgucijEgbneYUFVPZwL2sZNSIB4qfWH8gwiSLGNukMusGsSitR2E9HUu+etWg1Ab+7fVH3B3ci9+M93Jxz7EGHDuruJ6kAZtcLSqIYdm/S580gNcXvpxl7HtbikVp6exss5FmIFhz6+NadFjRzz+8hBUtJ5s+RL6ygcCcLspMh2edEUsV8/RMjJftbQ== hdi`430007270@H37392LCOLEO"
 
 $RGName="$ServiceName-$EnviromentName-rg"
@@ -40,6 +41,7 @@ az vm create `
 	--size $Size `
 	--authentication-type  $AuthType `
 	--admin-username $AdminUserName `
+  --admin-password $AdminPassword `
 	--ssh-key-values $SshPublicKey `
 	--storage-sku $StorageType `
 	--os-disk-size-gb $OsDiskSize
