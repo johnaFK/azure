722b03e606781f788d34f01c8324cc99a5d50e19
Johna <johnatan.jfk@gmail.com>
2021-07-01 16:06:08 -0500

    a

--- CLI Scripts/Create VM Script.ps1
+++ CLI Scripts/Create VM Script.ps1
@@ -6,7 +6,7 @@ $ImageUrnAlias="CentOS"
 $StorageType="Standard_LRS"
 $OsDiskSize="64"
 $Size="Standard_B2s"
-$AuthType="all"
+$AuthType="ssh"
 $AdminUserName="linadmin"
 $AdminPassword="abcDEF1029;1"
 $SshPublicKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDQl+aQ/wEjfcFO6GU2f7vWQHbgkSnZiDqIaRoJYSgxR71bXc5TTCfwlBamfl3Y930a6e6VPT/MYEM+gc4qfnulHMGmFOfrDr4qh3yItBa1zC0ZeRluYvYYs7Ujt/qS2rf/ZfxE3R+VAwKVYE+8dMeiEfiY5fPtdxo8cTA3O9f/rvNFf1H+anshNpJ6BR4Dnz4w7Phm5r+zJDkbuIz9+qTQcD3Yin95fmBptv9FK1KvhdVsA9F+ApsyWrC9BYBOWpK3Ee+/OxejlBwjUyh3Z5pc52sFz0H1VyytQzph2xXmcCCxfR9tyg3m4S0q9EdRWsu62hsdZAsZI6vN8lOztk6jC9JQCWw1bn9ocfTghoE7cM4nmZWiy5AZEtDyZVmDjDOJ2Xd1plg/z1f/HNAyEXnmT32vEy0KkUvNntRiarlFOBtSj+K1pBbn5IodKeVk8FdJsmiWArwFoW7N012zTXScCOGzQrxizgZPKZSTo/B5YjZz5FRIgucijEgbneYUFVPZwL2sZNSIB4qfWH8gwiSLGNukMusGsSitR2E9HUu+etWg1Ab+7fVH3B3ci9+M93Jxz7EGHDuruJ6kAZtcLSqIYdm/S580gNcXvpxl7HtbikVp6exss5FmIFhz6+NadFjRzz+8hBUtJ5s+RL6ygcCcLspMh2edEUsV8/RMjJftbQ== hdi`430007270@H37392LCOLEO"
@@ -41,10 +41,10 @@ az vm create `
 	--size $Size `
 	--authentication-type  $AuthType `
 	--admin-username $AdminUserName `
-  --admin-password $AdminPassword `
 	--ssh-key-values $SshPublicKey `
 	--storage-sku $StorageType `
 	--os-disk-size-gb $OsDiskSize
+  #--admin-password $AdminPassword `
 	
 echo "----------------------------------------------------------"
 echo "|                                                         |"
@@ -62,7 +62,7 @@ az network nsg rule create `
   --destination-port-ranges 80 `
   --access Allow
 
-  az network nsg rule create `
+az network nsg rule create `
   --resource-group $RGName `
   --nsg-name $NSGname `
   --name allow-http `
@@ -71,7 +71,7 @@ az network nsg rule create `
   --destination-port-ranges 8080 `
   --access Allow
 
-  az network nsg rule create `
+az network nsg rule create `
   --resource-group $RGName `
   --nsg-name $NSGname `
   --name allow-http `
@@ -80,7 +80,7 @@ az network nsg rule create `
   --destination-port-ranges 8001 `
   --access Allow
 
-  az network nsg rule create `
+az network nsg rule create `
   --resource-group $RGName `
   --nsg-name $NSGname `
   --name allow-http `
--- CLI Scripts/install-drools-on-linux.sh
+++ CLI Scripts/install-drools-on-linux.sh
@@ -3,5 +3,5 @@
 # Install Drools
 sudo docker run -p 8080:8080 -p 8001:8001 -d --name drools-wb jboss/business-central-workbench-showcase:latest
 
-# Install Kie Server
+# Install Kie Server and Link with drools-wb
 sudo docker run -p 8180:8080 -d --name kie-server --link drools-wb:kie-wb jboss/kie-server-showcase:latest
